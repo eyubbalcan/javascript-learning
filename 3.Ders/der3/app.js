bf66447556609dfeefb07e1a82848b5304cb6125
eyubbalcan <balcan1905@gmail.com>
2023-03-31 12:08:31 +0300

    yeni

--- 3.Ders/der3/app.js
+++ 3.Ders/der3/app.js
@@ -1,7 +1,15 @@
-console.log('hello')
-console.log('hello')
-console.log('hello')
-console.log('hello')
-console.log('hello')
-console.log('hello')
-console.log('hello')
+// let sayi1 = 5;
+// let sayi2 =10;
+
+// console.log(sayi1*sayi2)
+
+
+// let no1= 100;
+// let no2= 70;
+
+// let sonuc = (no1+no2)/2;
+
+// console.log(sonuc)
+
+// console.log(17%5)
+
