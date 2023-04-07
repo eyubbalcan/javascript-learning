1c3bafe1d9b4604f78f6b571a32f96f64c769c2a
eyubbalcan <balcan1905@gmail.com>
2023-04-07 13:38:54 +0300

    Methotlar

---
+++ 3.Ders/der3/metotlar.js
@@ -0,0 +1,19 @@
+
+// Parametresiz
+function yazdir(){
+    debugger;
+    console.log("Eyüp Balcan")
+}
+
+debugger;
+yazdir;
+
+
+// Parametteli
+function yazdir1(isim,soyisim){
+    debugger;
+    console.log(isim+ " " + soyisim);
+}
+
+debugger;
+yazdir1;
